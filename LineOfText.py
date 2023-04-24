e3c7f15350de0ffdeccf9c07842048ec4c3b565a
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 14:07:13 +0800

    Pseudocode

---
+++ LineOfText.py
@@ -0,0 +1,10 @@
+#Dahan,Regine Fae M. BSCPE 1-5 File Handling No. 3
+
+#open mylife.txt
+#user's input line
+#   write in mylife.txt
+#asking another for a possible user's input line
+#   if yes
+#   if no
+#display the output
+
