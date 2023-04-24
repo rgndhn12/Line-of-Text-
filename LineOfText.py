40f6789ec504b29252b380b7bf09257d4ed8a207
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 15:03:40 +0800

    If y, enter line again!

--- LineOfText.py
+++ LineOfText.py
@@ -1,19 +1,26 @@
 #Dahan,Regine Fae M. BSCPE 1-5 File Handling No. 3
 
+def multiple():
 
-#open mylife.txt
-with open ("mylife.txt", "w") as user_input_line:
-        
-#user's input line
-    line_content = input ("Enter line: ")
+    #open mylife.txt
+    with open ("mylife.txt", "w") as user_input_line:
+            
+    #user's input line
+        line_content = input ("Enter line: ")
 
-#   write in mylife.txt
-    user_input_line.write(line_content)
+    #   write in mylife.txt
+        user_input_line.write(line_content)
 
-#asking another for a possible user's input line
-want_another = input ("Are there more lines y/n? ")
-    
+    while True:  
+
+    #asking another for a possible user's input line
+        want_another = input ("Are there more lines y/n? ") 
+
     #   if yes
+        if (want_another.lower() == "y"):
+            multiple()
     #   if no
+    
     #display the output
 
+multiple()
