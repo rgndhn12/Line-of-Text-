838b5d304a9b89ce280aaf72498d7c49863ab691
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 15:43:33 +0800

    Adding features(Intro): Pyfiglet, Time, Colorama

--- LineOfText.py
+++ LineOfText.py
@@ -1,12 +1,26 @@
 #Dahan,Regine Fae M. BSCPE 1-5 File Handling No. 3
 
+#introduction
+import pyfiglet
+import time
+import colorama
+from colorama import Fore
+colorama.init()
+print(Fore.GREEN+pyfiglet.figlet_format(" HI ",font="isometric1"))
+time.sleep(1)
+border = "—" * 115
+print(border)
+print(Fore.CYAN+' This Program is entitled Line Of Text. The input lines by the user will go to a file named "mylife.txt".')
+print(border)
+time.sleep(2)
+
 def multiple():
 
     #open mylife.txt
     with open ("mylife.txt", "a") as user_input_line:
             
     #user's input line
-        line_content = input ("Enter line: ")
+        line_content = input ("Enter line:")
 
     #   write in mylife.txt
         user_input_line.write(line_content +"\n")
