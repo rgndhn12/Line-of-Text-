72a09ed4859d29b949077c2543f9a4e81aebbe25
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 14:32:43 +0800

    The user can now input another line!

--- LineOfText.py
+++ LineOfText.py
@@ -1,16 +1,19 @@
 #Dahan,Regine Fae M. BSCPE 1-5 File Handling No. 3
 
+
 #open mylife.txt
 with open ("mylife.txt", "w") as user_input_line:
-    
+        
 #user's input line
     line_content = input ("Enter line: ")
 
 #   write in mylife.txt
     user_input_line.write(line_content)
-    
+
 #asking another for a possible user's input line
-#   if yes
-#   if no
-#display the output
+want_another = input ("Are there more lines y/n? ")
+    
+    #   if yes
+    #   if no
+    #display the output
 
