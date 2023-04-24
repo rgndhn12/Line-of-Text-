c2007756b2b83fd6f37b78db7dedcd73838aef72
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 14:21:52 +0800

    Opening mylife.txt!

--- LineOfText.py
+++ LineOfText.py
@@ -1,10 +1,11 @@
 #Dahan,Regine Fae M. BSCPE 1-5 File Handling No. 3
 
 #open mylife.txt
-#user's input line
+with open ("mylife.txt", "w") as user_input_line:
+    
+#user's input line  
 #   write in mylife.txt
 #asking another for a possible user's input line
 #   if yes
 #   if no
 #display the output
-
