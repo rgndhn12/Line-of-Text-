54b281f50603cc8a98a67c69ab3fb7a1f86e588b
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 14:26:36 +0800

    The user's input is now in mylife.txt!

--- LineOfText.py
+++ LineOfText.py
@@ -5,7 +5,10 @@ with open ("mylife.txt", "w") as user_input_line:
     
 #user's input line
     line_content = input ("Enter line: ")
+
 #   write in mylife.txt
+    user_input_line.write(line_content)
+    
 #asking another for a possible user's input line
 #   if yes
 #   if no
