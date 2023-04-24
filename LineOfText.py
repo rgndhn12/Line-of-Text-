bf6c4bb7e233d19fab7d2b5c2c1c437bf70384dd
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 15:31:35 +0800

    The input lines by the user is now in mylife.txt!

--- LineOfText.py
+++ LineOfText.py
@@ -3,30 +3,28 @@
 def multiple():
 
     #open mylife.txt
-    with open ("mylife.txt", "w") as user_input_line:
+    with open ("mylife.txt", "a") as user_input_line:
             
     #user's input line
         line_content = input ("Enter line: ")
 
     #   write in mylife.txt
-        user_input_line.write(line_content)
-  
-    while True:  
+        user_input_line.write(line_content +"\n")
+
+    while True: 
 
     #asking another for a possible user's input line
-        want_another = input ("Are there more lines y/n? ") 
-
+        want_another = input ("Are there more lines y/n? ")
+ 
     #   if yes
         if (want_another.lower() == "y"):
             multiple()
     #   if no
         elif (want_another.lower() == "n"):
-            print("You can now view your input lines in mylife.txt!")
+            print("You can now view your input line in mylife.txt!")
             exit()
-        
         else:
             print ("Invalid.")
-            
-    #display the output
-
+            break
+    
 multiple()
